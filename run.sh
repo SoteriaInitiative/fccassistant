31f5ef454fba98bb0ee4564f18637ce3e69f3807
Jean Voigt <bvoigt@gmail.com>
2025-08-13 14:54:25 +0200

    Updated run configuration

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ python -m model.embed_and_index
 python -m model.generate_tuning_data
 python -m model.tune_gemini.py
 python -m app.query
+echo "For app deployment see commands and setup in app/README.md"
